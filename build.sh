6ef8922e6e6d2c7a162deab7c305105143456404
Maximo Pere <maximo.pere@bue.edu.ar>
2023-12-21 02:01:24 -0300

    Resubiendo los Asset

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-source venv/Scripts/activate
+venv\Scripts\activate
 pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
 reflex export --frontend-only
 mkdir public
-move C:\Users\usuario\proyects\my_web\frontend.zip C:\Users\usuario\proyects\my_web\public
+move C:\Users\usuario\proyects\git\my_web\frontend.zip C:\Users\usuario\proyects\git\my_web\public
 cd public
 tar -xvf frontend.zip
 del frontend.zip
